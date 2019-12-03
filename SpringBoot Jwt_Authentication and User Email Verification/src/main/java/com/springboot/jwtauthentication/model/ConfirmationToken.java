3d69c04c584d78f88a2e2f4b365ddc9208554054
RangaJ96 <49330699+RangaJ96@users.noreply.github.com>
2019-12-03 08:36:00 +0530

    Update ConfirmationToken.java

--- SpringBoot Jwt_Authentication and User Email Verification/src/main/java/com/springboot/jwtauthentication/model/ConfirmationToken.java
+++ SpringBoot Jwt_Authentication and User Email Verification/src/main/java/com/springboot/jwtauthentication/model/ConfirmationToken.java
@@ -29,8 +29,8 @@ public class ConfirmationToken {
 	private Date createdDate;
 	
 	@OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
-    @JoinColumn(nullable = false, name = "user_id")
-    private User user;
+    	@JoinColumn(nullable = false, name = "user_id")
+    	private User user;
 	
 	public ConfirmationToken() {
 	}
